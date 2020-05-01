2fbb59d0f4d0b02809ebd12b0b514ef946c4355f
CharlZ <charles.ceo2020@gmail.com>
2020-05-01 10:04:39 -0600

    first commit

---
+++ api/pets.php
@@ -0,0 +1,19 @@
+<?php
+$pets = array(
+    'Bear' => array(
+        'age' => 4,
+        'type' => 'cat',
+        'tricks' => array(
+            'fetch',
+            'roll-over'
+        )
+    )
+    'Lucas' => array(
+        'age' => 2,
+        'type' => 'newt',
+        'tricks' => array(
+            'hold',
+            'roll-over'
+        )
+    )
+);
